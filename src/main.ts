9ae71bc2d5e215916f467bcab3dceffad61f8519
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-09-19 19:36:14 +0200

    up

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store/store";
+
+import Vuetify from "vuetify";
+Vue.use(Vuetify);
+
+import "./style.scss";
+
+import { AxiosPlugin } from "./axios-plugin";
+
+// pass in your custom options as second parameter if applicable
+Vue.use(AxiosPlugin);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
